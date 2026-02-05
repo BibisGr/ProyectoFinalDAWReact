587ab7c8e8fafda03ad0d88edcbf0a7b96b6234e
Bibis Gr <bertha.garrastacho@digitechfp.com>
2026-02-05 12:27:41 +0100

    5taa fase Interfaces.ts y modify PeliculaCard.tsx

---
+++ ProyectoFinalReact/src/common/Interfaces.ts
@@ -0,0 +1,19 @@
+export interface ApiResponse {
+    status: Movie[]
+}
+
+export interface Movie {
+    imdb: Imdb
+    _id: string
+    title: string
+    year: number
+    director: string
+    plot: string
+    genres: string[]
+    poster: string
+}
+
+export interface Imdb {
+    rating: number
+    votes: number
+}
--- ProyectoFinalReact/src/components/PeliculaCard.tsx
+++ ProyectoFinalReact/src/components/PeliculaCard.tsx
@@ -1,15 +1,36 @@
+import {useNavigate} from "react-router-dom";
+
 type MovieProps ={
+    id:string;
     title: string;
-    price: number;
-    image: string;
+    year: number
+    poster: string;
+    director: string;
 }
-export default function PeliculaCard({title, price, image}: MovieProps) {
+export function PeliculaCard({title, year, poster, director}: MovieProps) {
     return (
         <article className="card p-3 cardPeli" style={{width: "20rem"}}>
             <h3 className="card-title">{title}</h3>
-            <img  className="rounded-4" src={image} alt={title} />
-            <p className="text-success">Price: ${price.toFixed(2)}</p>
-            <button className="buttonBuy">Comprar Tickets</button>
+            <img  className="rounded-4" src={poster} alt={title} />
+            <p className="text-success">Year: {year}</p>
+            <p className="text-success">Director: {director}</p>
+            <button className="buttonBuy">Ver Detalles</button>
+        </article>
+    )
+}
+export function PeliculaCardAPI({id,title, year, poster, director}: MovieProps) {
+    const navigate = useNavigate();
+
+    const handleClick = () =>{
+        navigate("/movie/" + id);
+    }
+    return (
+        <article className="card p-3 cardPeli" style={{width: "20rem"}}>
+            <h3 className="card-title">{title}</h3>
+            <img  className="rounded-4" src={poster} alt={title} />
+            <p className="text-success">Year: {year}</p>
+            <p className="text-success">Director: {director}</p>
+            <button onClick={handleClick} className="buttonBuy">Ver Detalles</button>
         </article>
     )
 }
